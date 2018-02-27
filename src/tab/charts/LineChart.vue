5d816e36c0215e31812becf861483b6622273611
adam <adam@adamharpur.com>
2018-02-27 11:35:30 -0600

    chore(*):refactor and experiments

---
+++ src/tab/charts/LineChart.vue
@@ -0,0 +1,22 @@
+<script>
+    import { Line } from 'vue-chartjs'
+
+    export default {
+        extends: Line,
+        created(){
+
+        },
+        mounted() {
+            this.renderChart({
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [
+                    {
+                    label: 'GitHub Commits',
+                    borderColor: '#5B105B',
+                    data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+                    }
+                ]
+            })
+        }
+    }
+</script>
